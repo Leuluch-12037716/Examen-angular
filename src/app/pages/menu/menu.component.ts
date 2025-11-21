dd420b29e7862858efbee01b11450d6c291cd551
Leuluch-12037716 <117209190+Leuluch-12037716@users.noreply.github.com>
2025-11-21 16:03:07 -0400

    Dark and light mode

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -10,5 +10,27 @@ export class MenuComponent {
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+    
   }
+  theme: string = 'light';
+
+  constructor() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+      this.theme = savedTheme;
+      document.documentElement.setAttribute('data-theme', savedTheme);
+    } else {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      this.theme = prefersDark ? 'dark' : 'light';
+      document.documentElement.setAttribute('data-theme', this.theme);
+    }
+  }
+
+  toggleTheme() {
+    this.theme = this.theme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', this.theme);
+    localStorage.setItem('theme', this.theme);
+  }
+  
 }
